059af8dbae8ed4ed522161f6108511f546760f69
Ifeanyichukwu Miracle <ifeanyimiraclechuks@gmail.com>
2024-10-21 20:00:58 +0100

    login functionality change

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -334,34 +334,35 @@ const Header = () => {
             Logout
           </p>
         ) : (
-          <p
-            className="menu-login"
-            title="logout"
-            onClick={logout}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              fontWeight: "500",
-              gap: ".3rem",
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.9}
-              stroke="currentColor"
-              className="size-6"
-              style={{ width: "1.3rem" }}
+          <Link to={`/login`}>
+            <p
+              className="menu-login"
+              title="login"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                fontWeight: "500",
+                gap: ".3rem",
+              }}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
-              />
-            </svg>
-            Login
-          </p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.9}
+                stroke="currentColor"
+                className="size-6"
+                style={{ width: "1.3rem" }}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+                />
+              </svg>
+              Login
+            </p>
+          </Link>
         )}
       </section>
     </>
