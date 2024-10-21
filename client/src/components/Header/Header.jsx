ae7cfde6665e0a21632a8aef5e30674adff192b4
Ifeanyichukwu Miracle <ifeanyimiraclechuks@gmail.com>
2024-10-21 19:58:01 +0100

    logout functionality change

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -304,34 +304,65 @@ const Header = () => {
             Search
           </li>
         </ul>
-        <p
-          className="menu-logout"
-          title="logout"
-          onClick={logout}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            fontWeight: "500",
-            gap: ".3rem",
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.9}
-            stroke="currentColor"
-            className="size-6"
-            style={{ width: "1.3rem" }}
+        {loggedIn ? (
+          <p
+            className="menu-logout"
+            title="logout"
+            onClick={logout}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              fontWeight: "500",
+              gap: ".3rem",
+            }}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
-            />
-          </svg>
-          Logout
-        </p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.9}
+              stroke="currentColor"
+              className="size-6"
+              style={{ width: "1.3rem" }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+              />
+            </svg>
+            Logout
+          </p>
+        ) : (
+          <p
+            className="menu-login"
+            title="logout"
+            onClick={logout}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              fontWeight: "500",
+              gap: ".3rem",
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.9}
+              stroke="currentColor"
+              className="size-6"
+              style={{ width: "1.3rem" }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+              />
+            </svg>
+            Login
+          </p>
+        )}
       </section>
     </>
   );
